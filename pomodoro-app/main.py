ee6323485320246a27057c7ae6dfddac1f656b58
prateekvarma <prtk.varma@gmail.com>
2023-01-09 16:42:15 -0600

    Count down timer functionality done

--- pomodoro-app/main.py
+++ pomodoro-app/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -15,13 +16,16 @@ LONG_BREAK_MIN = 20
 
 
 def start_timer():
-    count_down(5)
+    count_down(5 * 60) # minutes x 60 = seconds
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
 # ---------------------------- UI SETUP ------------------------------- #
