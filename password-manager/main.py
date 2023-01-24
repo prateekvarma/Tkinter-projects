deaa6acadd503f653016053ed41140fa163dd20c
prateekvarma <prtk.varma@gmail.com>
2023-01-23 22:30:12 -0600

    Added labels, entires & buttons with grid

--- password-manager/main.py
+++ password-manager/main.py
@@ -6,11 +6,35 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(pady=20, padx=20)
+window.config(pady=50, padx=50)
 
 canvas = Canvas(width=200, height=200)
 logo = PhotoImage(file="logo.png")  # extract from file system
 canvas.create_image(100, 100, image=logo)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website")
+website_label.grid(column=0, row=1)
+
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+email_label = Label(text="Email/Username")
+email_label.grid(column=0, row=2)
+
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2)
+
+password_label = Label(text="Password")
+password_label.grid(column=0, row=3)
+
+password_entry = Entry(width=18)
+password_entry.grid(column=1, row=3)
+
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
