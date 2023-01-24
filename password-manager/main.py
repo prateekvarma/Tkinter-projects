f640fe3b573b3e29d05417e6b11b7b67023700a7
prateekvarma <prtk.varma@gmail.com>
2023-01-24 16:40:57 -0600

    Set initial focus to an entry, and set inital values to another entry

--- password-manager/main.py
+++ password-manager/main.py
@@ -18,12 +18,14 @@ website_label.grid(column=0, row=1)
 
 website_entry = Entry(width=35)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()  # sets the cursor as initial position
 
 email_label = Label(text="Email/Username")
 email_label.grid(column=0, row=2)
 
 email_entry = Entry(width=35)
 email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "prateek@gmail.com")  # prefill text, starting from position 0
 
 password_label = Label(text="Password")
 password_label.grid(column=0, row=3)
