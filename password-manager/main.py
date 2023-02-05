e811af124d6db8d15f50b63df2dbd93ae5387525
prateekvarma <prtk.varma@gmail.com>
2023-02-05 13:42:00 -0600

    used pyperclip to copy password to clipboard

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
@@ -35,6 +36,7 @@ def generate_password():
     password = "".join(password_list)
 
     password_entry.insert(0, password)  # writing it to the field
+    pyperclip.copy(password)  # Copy the password to clipboard
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
