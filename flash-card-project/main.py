9bc910c9086b7bfc5d66078cbbc61e06e882ced6
prateekvarma <prtk.varma@gmail.com>
2023-03-02 16:10:48 -0600

    Changing card background and text color after flipping

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -19,8 +19,9 @@ def next_card():
 
 
 def flip_card():
-    canvas.itemconfig(card_title, text="English", fill="black")
-    canvas.itemconfig(card_word, text=current_card["English"], fill="black")
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
 window = Tk()
@@ -31,7 +32,8 @@ window.after(3000, func=flip_card)  # after 3000 ms, flip the card
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"), fill="black")
 card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"), fill="black")
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
